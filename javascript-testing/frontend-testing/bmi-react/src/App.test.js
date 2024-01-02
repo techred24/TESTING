76b81536e026eb97fb2e526c9583e72512d0dd6b
Rafael Temich <f.rafael.temich24@gmail.com>
2024-01-02 16:03:56 -0600

    Adding tests to verify weights from normal to obesity

--- javascript-testing/frontend-testing/bmi-react/src/App.test.js
+++ javascript-testing/frontend-testing/bmi-react/src/App.test.js
@@ -27,3 +27,54 @@ test('calculate Thinness result', () => {
   expect(bmiResult).toBeInTheDocument()
   expect(bmiEstimation).toBeInTheDocument()
 })
+
+test('calculate Normal result', () => {
+  const weightEl = screen.getByLabelText('Weight (KG)')
+  const heightEl = screen.getByLabelText('Height (M)')
+  const sendBtnEl = screen.getByRole('button', {name: /send/i})
+
+  weightEl.value = '80'
+  heightEl.value = '1.8'
+
+  fireEvent.click(sendBtnEl)
+
+  const bmiResult = screen.queryByText(/bmi: 24.691/i)
+  const bmiEstimation = screen.queryByText(/bmi estimation: normal/i)
+
+  expect(bmiResult).toBeInTheDocument()
+  expect(bmiEstimation).toBeInTheDocument()
+})
+
+test('calculate Overweight result', () => {
+  const weightEl = screen.getByLabelText('Weight (KG)')
+  const heightEl = screen.getByLabelText('Height (M)')
+  const sendBtnEl = screen.getByRole('button', {name: /send/i})
+
+  weightEl.value = '90'
+  heightEl.value = '1.8'
+
+  fireEvent.click(sendBtnEl)
+
+  const bmiResult = screen.queryByText(/bmi: 27.778/i)
+  const bmiEstimation = screen.queryByText(/bmi estimation: overweight/i)
+
+  expect(bmiResult).toBeInTheDocument()
+  expect(bmiEstimation).toBeInTheDocument()
+})
+
+test('calculate Obese result', () => {
+  const weightEl = screen.getByLabelText('Weight (KG)')
+  const heightEl = screen.getByLabelText('Height (M)')
+  const sendBtnEl = screen.getByRole('button', {name: /send/i})
+
+  weightEl.value = '100'
+  heightEl.value = '1.8'
+
+  fireEvent.click(sendBtnEl)
+
+  const bmiResult = screen.queryByText(/bmi: 30.864/i)
+  const bmiEstimation = screen.queryByText(/bmi estimation: obese/i)
+
+  expect(bmiResult).toBeInTheDocument()
+  expect(bmiEstimation).toBeInTheDocument()
+})
